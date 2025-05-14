2bd1dd75231eff9c285e81449fa880521ad048d0
mlm mustard 47 <mlpezmn@gmail.com>
2025-05-14 21:19:13 +0200

    idk pretty much nothing

---
+++ script.js
@@ -0,0 +1,28 @@
+const oneBtn = document.getElementById("n1");
+const twoBtn = document.getElementById("n2");
+const threeBtn = document.getElementById("n3");
+const fourBtn = document.getElementById("n4");
+const fiveBtn = document.getElementById("n5");
+const sixBtn = document.getElementById("n6");
+const sevenBtn = document.getElementById("n7");
+const eightBtn = document.getElementById("n8");
+const nineBtn = document.getElementById("n9");
+const zeroBtn = document.getElementById("n0");
+const decimalBtn = document.getElementById("decimal");
+const piBtn = document.getElementById("pi");
+
+const addBtn = document.getElementById("add");
+const substractBtn = document.getElementById("substract");
+const multiplyBtn = document.getElementById("multiply");
+const divideBtn = document.getElementById("divide");
+const equalsBtn = document.getElementById("equals");
+
+const clearBtn = document.getElementById("clear");
+const deleteBtn = document.getElementById("delete");
+
+const operationContainer = document.querySelector(".operation-container");
+const resultContainer = document.querySelector(".result-container");
+
+const operate = (num1,num2)=>{
+    
+}
